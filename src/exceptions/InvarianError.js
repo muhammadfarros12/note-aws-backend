b24aec016474f9aba9817459c273efec9c0550fc
muhamad farroos <muhammadfarroos12@gmail.com>
2021-09-06 21:15:14 +0700

    commit for checking error

--- src/exceptions/InvarianError.js
+++ src/exceptions/InvarianError.js
@@ -1,10 +1,10 @@
 const ClientError = require("./ClientError");
 
-class InvarianError extends ClientError{
+class InvariantError extends ClientError{
     constructor(message){
         super(message);
         this.name = 'InvarianError';
     }
 }
 
-module.exports = InvarianError;
+module.exports = InvariantError;
--- src/server.js
+++ src/server.js
@@ -5,11 +5,18 @@ const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 // kita ganti dengan yg baru dikarenakan kita langsung menggunakan database tanpa memori note
 //const NotesService = require('./services/inMemory/NotesService');
+// notes
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -22,13 +29,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+      {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
